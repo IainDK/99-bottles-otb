1e9865c1b77f52334919dc794b03f91de6c48511
Iain Kinnish <iainkinnish@Iains-MacBook-Pro.local>
2017-09-25 10:32:34 +0100

    Passed test number 4.

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -1,8 +1,10 @@
 class Bottles
 
   def verse(number)
-    if number > 1
+    if number > 2
       "#{number} bottles of beer on the wall, #{number} bottles of beer.\nTake one down and pass it around, #{number-1} bottles of beer on the wall.\n"
+    elsif number == 2
+      "#{number} bottles of beer on the wall, #{number} bottles of beer.\nTake one down and pass it around, #{number-1} bottle of beer on the wall.\n"
     else
       "#{number} bottle of beer on the wall, #{number} bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
     end
